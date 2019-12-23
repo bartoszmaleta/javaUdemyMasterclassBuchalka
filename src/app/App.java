29d5476f58a5b1d4c844c7b2d414af813a8527a0
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-23 20:46:16 +0100

    Add another variable

--- src/app/App.java
+++ src/app/App.java
@@ -4,8 +4,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Bartosz");
 
-        int myFirstNumber = 5;
-         
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        
+        System.out.println("myFirstNumber");
         System.out.println(myFirstNumber);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
     }
 }
