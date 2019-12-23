14495402d78547436062255ffac24a05bbcc10de
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-23 20:29:07 +0100

    Add first variable and print it

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Hello Bartosz");
+
+        int myFirstNumber = 5;
+         
+        System.out.println(myFirstNumber);
     }
 }
