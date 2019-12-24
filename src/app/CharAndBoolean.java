b4ce0aaf928c5d021eb5b470bcb5b7f22685fe4d
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-24 14:26:25 +0100

    Add one variable of char

--- src/app/CharAndBoolean.java
+++ src/app/CharAndBoolean.java
@@ -2,6 +2,12 @@ package app;
 
 public class CharAndBoolean {
     public static void main(String[] args) {
+        System.out.println("---------------------------");
+        
+        char myChar = 'D';
+
+        
+
 
     }
 }
