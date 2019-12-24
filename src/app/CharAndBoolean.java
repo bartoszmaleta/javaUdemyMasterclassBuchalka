07dbcf6496d89164e67f89bef547329f63c1dfe9
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-24 14:41:42 +0100

    Add unicode characters

--- src/app/CharAndBoolean.java
+++ src/app/CharAndBoolean.java
@@ -5,8 +5,19 @@ public class CharAndBoolean {
         System.out.println("---------------------------");
         
         char myChar = 'D';
+        char myUnicodeChar = '\u0044';
 
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);        
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        System.out.println("---------------------------");
         
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
 
 
     }
