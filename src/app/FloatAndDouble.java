ec7e4574a62d392f0127cbf98880838dc91ba123
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-24 12:52:28 +0100

    Add float and double max and min values

--- src/app/FloatAndDouble.java
+++ src/app/FloatAndDouble.java
@@ -2,6 +2,21 @@ package app;
 
 public class FloatAndDouble {
     public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+
+        System.out.println("---------------------------");
+
 
     }
 }
