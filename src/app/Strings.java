e535fd13b249b4c3d995e59bf40a011e5ad30e9b
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-28 00:01:41 +0100

    Add strings with ints and double

--- src/app/Strings.java
+++ src/app/Strings.java
@@ -8,11 +8,28 @@ public class Strings {
         myString = myString + ", and this is more.";
         System.out.println("myString is euqal to " + myString);
 
+        System.out.println("---------------------------");
+
         myString = myString + " \u00A9 2019";
         System.out.println("myString is equal to " + myString);
+
+        System.out.println("---------------------------");
 
         String numberString = "250.55";
         numberString = numberString + "49.95";
         System.out.println(numberString);
+
+        System.out.println("---------------------------");
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+
+        System.out.println("---------------------------");
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+
     }
 }
