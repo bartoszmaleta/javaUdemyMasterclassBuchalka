68b16dbc8519f33ab453b1208cdfe597eaeca276
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-29 00:29:26 +0100

    Add expression info

--- src/app/KeywordsAndExpressions.java
+++ src/app/KeywordsAndExpressions.java
@@ -2,6 +2,12 @@ package app;
 
 public class KeywordsAndExpressions {
     public static void main(String[] args) {
-        
+        double kilometres = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore == 50)  {
+            System.out.println("This is an expression"); // in brackets is expression
+        }
+        // without semicolon and brackets or braces
     }
 }
