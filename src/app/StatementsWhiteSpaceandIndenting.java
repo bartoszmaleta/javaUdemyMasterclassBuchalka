3f35ce474007e9e8df3356819de156f8fd69add3
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-29 00:32:11 +0100

    Add some statements

--- src/app/StatementsWhiteSpaceandIndenting.java
+++ src/app/StatementsWhiteSpaceandIndenting.java
@@ -3,6 +3,13 @@ package app;
 public class StatementsWhiteSpaceandIndenting {
     public static void main(String[] args) {
         int myVariable = 50;
+        myVariable++;
+        myVariable--;
         
+        System.out.println("this is a test");
+        
+        System.out.println("This is" + 
+        " another " + 
+        "still more.");
     }
 }
