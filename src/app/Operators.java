d3fd3edfed65b36ecc12b25beefbec2ea9897d0c
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-28 17:23:46 +0100

    Add changing variables in memory

--- src/app/Operators.java
+++ src/app/Operators.java
@@ -2,6 +2,22 @@ package app;
 
 public class Operators {
     public static void main(String[] args) {
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+
+        System.out.println("---------------------------");
+
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+
+        System.out.println("---------------------------");
+        
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);)
+    
+        System.out.println("---------------------------");
+        
         
     }
 }
