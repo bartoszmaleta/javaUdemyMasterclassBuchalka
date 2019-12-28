73812d18b98e71ea42732d8ff9aa0af447b668bf
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-28 18:16:28 +0100

    Add && and || operators

--- src/app/Operators.java
+++ src/app/Operators.java
@@ -31,7 +31,33 @@ public class Operators {
         result = result % 3;
         System.out.println("3 % 3 = " + result);
 
+        System.out.println("---------------------------"); 
+
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("It is not an alien!");      // just first line will be under if statement
+            System.out.println("Alien attacks");
+
         System.out.println("---------------------------");
+
+        int topScore = 100;
+        if (topScore >= 100) {       // OPTIONS:    >=, <, <=, ==, !=
+            System.out.println("You got the high score!");
+        }
+
+        System.out.println("---------------------------");
+        
+        int secondTopScore = 60;
+        if ((topScore > secondTopScore) && (topScore >= 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        System.out.println("---------------------------");
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+
+        }
 
     }
 }
