33e8272314f1d5776528ea2a1705b3b45640cc7b
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-29 23:31:48 +0100

    Add variables methods

--- src/app/Methods.java
+++ src/app/Methods.java
@@ -3,7 +3,18 @@ package app;
 public class Methods {
     public static void main(String[] args) {
         System.out.println("---------------------------");
- 
+
+        boolean gameOver = true;
+        int score = 200;
+        int levelCompleted = 6;
+        int bonus = 400;
+
+        System.out.println("---------------------------");
+        
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        System.out.println("---------------------------");
+
         calculateScore(true, 800, 5, 100);
 
         System.out.println("---------------------------");
@@ -11,7 +22,6 @@ public class Methods {
         calculateScore(true, 10000, 8, 200);
         
         System.out.println("---------------------------");
-
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -20,6 +30,5 @@ public class Methods {
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }   
-         
     }
 }
