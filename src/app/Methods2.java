7737d1100eba1b5592f2def3e58d885c6bbdd718
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-29 23:57:48 +0100

    Add two methods with parameters

--- src/app/Methods2.java
+++ src/app/Methods2.java
@@ -24,6 +24,15 @@ public class Methods2 {
         highScore = calculateScore(gameOver, score, levelCompleted, bonus);
         System.out.println("Your final score was " + highScore);
 
+        System.out.println("---------------------------");
+
+        displayHighScorePosition("Bartosz", 6);
+
+        calculateHighScorePosition(1500);
+        calculateHighScorePosition(900);
+        calculateHighScorePosition(400);
+        calculateHighScorePosition(50);
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -35,6 +44,22 @@ public class Methods2 {
         // } return -1;     // ANOTHER OPTION
         }  else {
             return -1;
+        }
+    }
+    
+    public static void displayHighScorePosition(String playerName, int positionOnHighScoreTable) {
+        System.out.println(playerName + " managed to get into position " + positionOnHighScoreTable + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore > 1000) {
+            return 1;
+        } else if ((playerScore > 500) && (playerScore < 1000)) {
+            return 2;
+        } else if ((playerScore > 100) && (playerScore < 500)) {
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
