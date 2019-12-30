a73f7844392477d02a6d779387d5c2ac0813f5bb
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-30 23:15:13 +0100

    Cosmetics

--- src/app/SpeedConverter.java
+++ src/app/SpeedConverter.java
@@ -1,9 +1,6 @@
 package app;
 
 public class SpeedConverter {
-    public static void main(String[] args) {
-
-    }
 
     public static long toMilesPerHour(double kilometersPerHour) {
         if (kilometersPerHour < 0) {
@@ -18,7 +15,7 @@ public class SpeedConverter {
             System.out.println("Invalid value");
         } else {
             long milesPerHour = toMilesPerHour(kilometersPerHour);
-            System.out.println(kilometersPerHour + " km/h " +  milesPerHour + " mi/h");
+            System.out.println(kilometersPerHour + " km/h = " +  milesPerHour + " mi/h");
         }
      }
 }
