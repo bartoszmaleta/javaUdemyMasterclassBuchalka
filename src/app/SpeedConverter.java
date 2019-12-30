2bdb374a3333bf1b9596e9dc522bc4b7d6c08bbd
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-30 23:24:41 +0100

    Add another solution

--- src/app/SpeedConverter.java
+++ src/app/SpeedConverter.java
@@ -2,12 +2,23 @@ package app;
 
 public class SpeedConverter {
 
+    // TESTING FUNCTION
+    // public static void main(String[] args) {
+    //     toMilesPerHour(75.114);
+    // }
+
     public static long toMilesPerHour(double kilometersPerHour) {
         if (kilometersPerHour < 0) {
             return -1;
         }
 
-        return Math.round(kilometersPerHour / 1.609);
+        double calculatedMiles = kilometersPerHour / 1.609;
+        long roundedMiles = Math.round(calculatedMiles);
+        // System.out.println(roundedMiles);  // redundat, just for test in main
+        return roundedMiles;
+
+        // ONE LINE SOLUTION:
+        // return Math.round(kilometersPerHour / 1.609);
     }
 
     public static void printConversion(double kilometersPerHour) {
