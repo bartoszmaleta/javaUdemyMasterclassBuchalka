ac116473eccecc3c22627c955269f09aa03e98dd
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-12-30 22:27:36 +0100

    Add checkNumberIfPositiveNegativeOrZero

--- src/app/PositiveNegativeZero.java
+++ src/app/PositiveNegativeZero.java
@@ -1,7 +1,20 @@
 package app;
 
 public class PositiveNegativeZero {
-    public static void checkNumber(int number) {
 
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(-10);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if (number == 0) {
+            System.out.println("zero");
+        }
     }
 }
