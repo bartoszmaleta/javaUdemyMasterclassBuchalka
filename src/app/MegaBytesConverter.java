bb6ea5f34fda0dafd476f8737b884862e0f2dd59
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-01 20:22:12 +0100

    Result consider 1024 kb in mb

--- src/app/MegaBytesConverter.java
+++ src/app/MegaBytesConverter.java
@@ -4,21 +4,21 @@ public class MegaBytesConverter {
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         if (kiloBytes < 0) {
             System.out.println("Invalid Value");
+        } else {
+
+            int remainingKilobytes = kiloBytes % 1024;
+            // System.out.println(remainingKilobytes);
+
+            int calculatedMegaBytes = (kiloBytes - remainingKilobytes) / 1000;
+            // System.out.println(calculatedMegaBytes);
+
+            System.out.println(kiloBytes + " KB = " + calculatedMegaBytes + " MB and " + remainingKilobytes + " KB");
         }
-        
-        int calculatedMegaBytes = kiloBytes / 1000;
-        System.out.println(calculatedMegaBytes);
-
-        int remainingKilobytes = kiloBytes - (calculatedMegaBytes * 1000);
-        System.out.println(remainingKilobytes);
-
-
-
     }
 
     public static void main(String[] args) {
 
-        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(5000);
     }
- 
+
 }
