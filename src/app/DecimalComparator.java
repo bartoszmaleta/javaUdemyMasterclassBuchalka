52397d0dcc336f0a0ef94b1651ee863318c947e7
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-03 00:05:38 +0100

    Add firstNumber calculating decimals

--- src/app/DecimalComparator.java
+++ src/app/DecimalComparator.java
@@ -1,14 +1,25 @@
 package app;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
+        System.out.println("firstNumber               = " + firstNumber);
         int realNumber = (int) firstNumber;
-        System.out.println(realNumber);
+        System.out.println("realNumber                = " + realNumber);
         double remainingOfNumber = firstNumber - realNumber;
-        System.out.println("Remaining:   " + remainingOfNumber);
+        System.out.println("remainingOfNumber         = " + remainingOfNumber);
 
-        // BigDecimal remainingOfNumberRounded = new BigDecimal(remainingOfNumber).setScale(3, RoundingMode.HALF_EVEN);
-        // remainingOfNumber = remainingOfNumberRounded.doubleValue();
+
+        double num = 1.34567;
+        DecimalFormat remainingOfNumberRounded = new DecimalFormat("#.###");
+        remainingOfNumberRounded.setRoundingMode(RoundingMode.FLOOR);
+        System.out.println("remainingOfNumberRounded  = " + remainingOfNumberRounded.format(remainingOfNumber));
+
+        // secondNumber:
+
+
 
         return true;
     }
