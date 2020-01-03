d2f911acf7884a213ceca0d6e1b052563a6a630e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-03 15:21:26 +0100

    Initial commit

---
+++ src/app/TeenNumberChecker.java
@@ -0,0 +1,15 @@
+package app;
+
+public class TeenNumberChecker {
+    public static boolean hasTeen(int firstNumber, int secondNumber, int thirdNumber) {
+
+        
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        boolean checkingAge = hasTeen(9, 99, 19);
+        System.out.println(checkingAge);
+    }
+}
