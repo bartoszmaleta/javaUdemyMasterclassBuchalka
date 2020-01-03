95a09709c1449f134e0959728278d6641b98d89f
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-03 15:10:06 +0100

    Initial commit

---
+++ src/app/EqualSumChecker.java
@@ -0,0 +1,11 @@
+package app;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int firstNumber, int secondNumber, int thirdNumber) {
+        return true;
+    }
+    
+    public static void main(String[] args) { 
+
+    }
+}
