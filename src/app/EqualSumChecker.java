07f08997e9137b2911bb079467ff4d36a6d91e49
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-03 15:16:12 +0100

    Add EqualSumChecker

--- src/app/EqualSumChecker.java
+++ src/app/EqualSumChecker.java
@@ -2,10 +2,24 @@ package app;
 
 public class EqualSumChecker {
     public static boolean hasEqualSum(int firstNumber, int secondNumber, int thirdNumber) {
-        return true;
+        int sumOfFirstAndSecond = firstNumber + secondNumber;
+
+        if (sumOfFirstAndSecond == thirdNumber) {
+            return true;
+        } else { 
+            return false;
+        }
     }
     
     public static void main(String[] args) { 
+    boolean sumOfOnesResultOne = hasEqualSum(1, 1, 1);
+    System.out.println(sumOfOnesResultOne);
+
+    boolean sumOfOnesResultTwo = hasEqualSum(1, 1, 2);
+    System.out.println(sumOfOnesResultTwo);
+
+    boolean sumOfOneAndMinusOneResultZero = hasEqualSum(1, -1, 0);
+    System.out.println(sumOfOneAndMinusOneResultZero);
 
     }
 }
