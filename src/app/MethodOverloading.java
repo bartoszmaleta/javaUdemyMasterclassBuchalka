0306df83d85c2531d44cde2d505eba68478727ab
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-06 14:34:38 +0100

    Add feet and inches to centimeters func

--- src/app/MethodOverloading.java
+++ src/app/MethodOverloading.java
@@ -1,5 +1,7 @@
 package app;
 
+// MethodOverloading - same method name, but different paramters
+
 public class MethodOverloading {
     public static void main(String[] args) {
 
@@ -9,6 +11,12 @@ public class MethodOverloading {
         calculateScore(75);
 
         calculateScore();
+
+        double inchesThirteen = calcFeetAndInchesToCentimeters(13);
+        System.out.println(inchesThirteen);
+
+        double inchesAndFeet = calcFeetAndInchesToCentimeters(2, 5);
+        System.out.println(inchesAndFeet);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -24,6 +32,39 @@ public class MethodOverloading {
     public static int calculateScore() {
         System.out.println("Unnamed player scored, no player score. ");
         return 0;
+    }
+
+    // Wrong! Changing return data type is not enough!
+    // public static void calculateScore() {
+        // System.out.println("Unnamed player scored, no player score. ");
+    // }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if (feet >= 0) {
+            feet = feet;
+        } else {
+            return -1;
+        }
+
+        if ((inches >= 0) && (inches <= 12)) {
+            inches = inches;
+        } else {
+            return -1;
+        }
+
+        double feetInInches = feet * 12;
+
+        double resultInCentimeters = (inches + feetInInches) * 2.54;
+        return resultInCentimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches >= 0) {
+            double feetInInches = inches / 12;
+            return feetInInches;
+        } else {
+            return -1;
+        }
     }
 
 }
