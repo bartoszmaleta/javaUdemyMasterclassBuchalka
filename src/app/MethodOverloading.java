6898d4b775dc90104a97968b0a6f3201a17b3154
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-06 14:53:43 +0100

    Change func with one paramter

--- src/app/MethodOverloading.java
+++ src/app/MethodOverloading.java
@@ -15,7 +15,7 @@ public class MethodOverloading {
         double inchesThirteen = calcFeetAndInchesToCentimeters(13);
         System.out.println(inchesThirteen);
 
-        double inchesAndFeet = calcFeetAndInchesToCentimeters(2, 5);
+        double inchesAndFeet = calcFeetAndInchesToCentimeters(6, 0);
         System.out.println(inchesAndFeet);
     }
 
@@ -36,40 +36,47 @@ public class MethodOverloading {
 
     // Wrong! Changing return data type is not enough!
     // public static void calculateScore() {
-        // System.out.println("Unnamed player scored, no player score. ");
+    // System.out.println("Unnamed player scored, no player score. ");
     // }
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
         if (feet >= 0) {
             feet = feet;
         } else {
+            System.out.println("Invalid valid feet or inches");
             return -1;
         }
 
         if ((inches >= 0) && (inches <= 12)) {
             inches = inches;
         } else {
+            System.out.println("Invalid valid feet or inches");
             return -1;
         }
 
-        // ONE LINE CONFIRMATION!!!! 
+        // ONE LINE CONFIRMATION!!!!
         // if ((feet < 0) || ((inches < 0) || (inches > 12))) {
-        //     return -1;
+        // System.out.println("Invalid valid feet or inches");
+        // return -1;
         // }
 
         double feetInInches = feet * 12;
 
         double resultInCentimeters = (inches + feetInInches) * 2.54;
+
+        System.out.println(feet + " feet, " + inches + " inches " + resultInCentimeters + "cm");
         return resultInCentimeters;
     }
 
     public static double calcFeetAndInchesToCentimeters(double inches) {
-        if (inches >= 0) {
-            double feetInInches = inches / 12;
-            return feetInInches;
-        } else {
+        if (inches < 0) {
             return -1;
         }
+
+        double feetInInches = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is euqal to " + feetInInches + " feet and " + remainingInches + " inches");
+        return feetInInches;
     }
 
 }
