fbb097f3ff0a772d6e10080107bc5f1bb6325811
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-06 20:49:59 +0100

    Cosmetics

--- src/controlFlowStatements/SumOddRange.java
+++ src/controlFlowStatements/SumOddRange.java
@@ -5,24 +5,24 @@ public class SumOddRange {
 
         int sumOfOddNumbers = sumOdd(1, 100);
         System.out.println(sumOfOddNumbers);
-        
+
         int sumOfOddNumbers2 = sumOdd(-1, 100);
         System.out.println(sumOfOddNumbers2);
-    
+
         int sumOfOddNumbers3 = sumOdd(100, 100);
         System.out.println(sumOfOddNumbers3);
-    
+
         int sumOfOddNumbers4 = sumOdd(13, 13);
         System.out.println(sumOfOddNumbers4);
-    
+
         int sumOfOddNumbers5 = sumOdd(1, -100);
         System.out.println(sumOfOddNumbers5);
-    
+
         int sumOfOddNumbers6 = sumOdd(100, 1000);
         System.out.println(sumOfOddNumbers6);
     }
 
-    public static boolean isOdd (int number) {
+    public static boolean isOdd(int number) {
         if (number < 0) {
             return false;
         }
@@ -37,8 +37,8 @@ public class SumOddRange {
     public static int sumOdd(int start, int end) {
         if ((end < start) || (start < 0) || (end < 0)) {
             return -1;
-        } 
-        
+        }
+
         int sumOfOddNumbers = 0;
         int counter = 0;
         for (int i = start; i <= end; i++) {
@@ -46,7 +46,6 @@ public class SumOddRange {
                 sumOfOddNumbers += i;
             }
         }
-
 
         return sumOfOddNumbers;
     }
