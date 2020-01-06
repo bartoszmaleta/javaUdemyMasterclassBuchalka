a2a3bca3e15814741d6d9921cc04722b6dc7025a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-06 15:30:06 +0100

    Add Seconds and minutes converter

---
+++ src/app/SecondsAndMinutesChallenge.java
@@ -0,0 +1,49 @@
+package app;
+
+public class SecondsAndMinutesChallenge {
+    public static void main(String[] args) {
+
+        String calculatedMinutesAndSeconds = getDurationString(110, 2);
+        System.out.println(calculatedMinutesAndSeconds);
+
+        String calculatedMinutesAndSecondsFromSecondsOnly = getDurationString(110);
+        System.out.println(calculatedMinutesAndSecondsFromSecondsOnly);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if ((minutes < 0) || (seconds < 0 || seconds >= 60)) {
+            String invalidValueMessage = "Invalid values";
+            return invalidValueMessage;
+        }
+
+        // To make seconds like minutes!
+        // int secondsToMinutes = seconds / 60;
+        // minutes = minutes + secondsToMinutes; # to make seconds like minutes!
+        int minutesToHours = minutes / 60;
+
+        int remainingSeconds = seconds % 60;
+        int remainingMinutes = minutes % 60;
+
+        if (minutesToHours < 10) {
+            String resultString = ("0" + minutesToHours + "h " + remainingMinutes + "m " + remainingSeconds + "s");
+            return resultString;
+
+        } else {
+            String resultString = (minutesToHours + "h " + remainingMinutes + "m " + remainingSeconds + "s");
+            return resultString;
+        }
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            String invalidValueMessage = "Invalid values";
+            return invalidValueMessage;
+        }
+
+        int minutes = seconds / 60;
+
+        int remainingSeconds = seconds % 60;
+
+        return getDurationString(minutes, remainingSeconds);
+    }
+}
