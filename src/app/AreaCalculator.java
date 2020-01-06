33a4509a98115fc6b86144d429566063b4594301
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-06 17:29:27 +0100

    Add areaOfCircle() and areaOfRectangle()

--- src/app/AreaCalculator.java
+++ src/app/AreaCalculator.java
@@ -5,5 +5,24 @@ public class AreaCalculator {
 
     }
 
+    public static double area (double radius) {
+        if (radius < 0) {
+            double valueError = -1.0;
+            return valueError; 
+        }
+        double areaOfCircle = Math.PI * (radius *radius);
+
+        return areaOfCircle;
+    }
+
+    public static double area (double x, double y) {
+        if ((x < 0) || (y < 0)) {
+            double valueError = -1.0;
+            return valueError;
+        }
+        
+        double areaOfRectangle = x * y;
+        return areaOfRectangle;
+    }
     
 }
