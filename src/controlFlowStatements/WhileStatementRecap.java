1e17dc23781f2966da11076987af40fc47b14b71
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-07 09:52:38 +0100

    Add example of while loop

---
+++ src/controlFlowStatements/WhileStatementRecap.java
@@ -0,0 +1,23 @@
+package controlFlowStatements;
+
+public class WhileStatementRecap {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number < 15) {
+            number++;
+
+            if (number <= 5) {
+                System.out.println("Skipping number " + number);
+                continue;
+            }
+
+            System.out.println("number = " + number);
+
+            if (number >= 10) {
+                System.out.println("Breaking at " + number);
+                break;
+            }
+        }
+        System.out.println("Out of loop");
+    }
+}
