eb9e04f7f9388269239d634a5071a128f07762a9
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-07 18:58:23 +0100

    Add basics of Scanner()

---
+++ src/controlFlowStatements/UserInputTutorial.java
@@ -0,0 +1,24 @@
+package controlFlowStatements;
+
+import java.util.Scanner;
+
+public class UserInputTutorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // handle next line character (enter key)
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2018 - yearOfBirth;
+
+        System.out.println("Your name is " + name + ", and You are " + age + " years old.");
+        
+        scanner.close();
+
+    }
+
+
+}
