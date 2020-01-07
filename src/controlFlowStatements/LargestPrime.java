4627cdd7700d1917dc3eede63906a8caecc76b6b
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-07 15:00:41 +0100

    Add getLargestPrime()

---
+++ src/controlFlowStatements/LargestPrime.java
@@ -0,0 +1,29 @@
+package controlFlowStatements;
+
+public class LargestPrime {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+
+        if (number >= 2) {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    number /= i;
+                    i--;
+                }
+            }
+            return number;
+        } else {
+            return -1;
+        }
+    }
+}
