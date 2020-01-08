2b83871e56017255c6976790b18f26e6b0f38df6
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-08 13:16:38 +0100

    Add constructors

--- src/oopPart1/bankAccount78video/BankAccount.java
+++ src/oopPart1/bankAccount78video/BankAccount.java
@@ -1,19 +1,51 @@
 package oopPart1.bankAccount78video;
 
 public class BankAccount {
-    private int accountNumber;
+
+    private String accountNumber;
     private double balance;
     private String customerName;
     private String customerEmail;
-    private int customerPhoneNumber;
+    private String customerPhoneNumber;
 
-    // private double depositedFunds;
+    public BankAccount() {
+        // "this..." ---> must be the first statement in constructor body 
+        this("123", 2.50, "Default name", "Default address", "Default phone");
+        System.out.println("Empty constructor called");
+    }    
 
-    public int getAccountNumber() {
+    public BankAccount(String accountNumber, double balance
+    , String customerName, String customerEmail
+    , String customerPhoneNumber) {
+        System.out.println("BankAccount constructor with parameters called");
+
+        // ONE APPROACH:
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhoneNumber = customerPhoneNumber;
+        
+        // SECOND APPROACH:
+        // setAccountNumber(accountNumber);
+        // setBalance(balance);
+        // setCustomerName(customerName);
+        // setCustomerEmail(customerEmail);
+        // setCustomerPhoneNumber(customerPhoneNumber);    
+    }
+
+    public BankAccount(String customerName, String customerEmail, String customerPhoneNumber) {
+        this("9999", 100.55, customerName, customerEmail, customerPhoneNumber);
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public String getAccountNumber() {
         return this.accountNumber;
     }
 
-    public void setAccountNumber(int accountNumber) {
+    public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
@@ -41,30 +73,29 @@ public class BankAccount {
         this.customerEmail = customerEmail;
     }
 
-    public int getCustomerPhoneNumber() {
+    public String getCustomerPhoneNumber() {
         return this.customerPhoneNumber;
     }
 
-    public void setCustomerPhoneNumber(int customerPhoneNumber) {
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
         this.customerPhoneNumber = customerPhoneNumber;
     }
 
-    // public double getDepositedFunds() {
-    // return this.depositedFunds;
-    // }
-    // public void setDepositedFunds(double depositedFunds) {
-    // this.depositedFunds = depositedFunds;
-    // }
-
     public void depositFund(double depositedFunds) {
         this.balance = balance + depositedFunds;
+        System.out.println("Deposit of " + depositedFunds + " processed.");
+        System.out.println("Right now You have = " + this.balance);
     }
 
     public void withdrawFunds(double withdrawalFunds) {
-        if (balance > withdrawalFunds) {
-            this.balance = balance - withdrawalFunds;
+        if (this.balance >= withdrawalFunds) {
+            this.balance -= withdrawalFunds;
+            System.out.println("You withdrawed = " + withdrawalFunds);
+            System.out.println("Remaining balance = " + this.balance);
         } else {
-            System.out.println("Not enough funds on account");
+            System.out.println("You tried to withdraw = " + withdrawalFunds);
+            System.out.println("Not enough funds on account.");
+            System.out.println("Right now You have = " + this.balance);
         }
     }
 }
--- src/oopPart1/bankAccount78video/MainBankAccount.java
+++ src/oopPart1/bankAccount78video/MainBankAccount.java
@@ -2,16 +2,52 @@ package oopPart1.bankAccount78video;
 
 public class MainBankAccount {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
+        // FIRST SOLUTION, WITHOUT CONSTRUCTOR IN BANKACCOUNT CLASS
+        // BankAccount bartoszSecondAccount = new BankAccount();
+// 
+        // System.out.println("-------------------");
+// 
+        // System.out.println(bartoszSecondAccount.getCustomerPhoneNumber());
+        // System.out.println(bartoszSecondAccount.getCustomerName());
+        // System.out.println(bartoszSecondAccount.getCustomerEmail());
+// 
+        System.out.println("-------------------");
+        BankAccount bartoszAccount= new BankAccount();
 
-        bankAccount.setBalance(5000);
-        System.out.println("Balance = " + bankAccount.getBalance());
 
-        bankAccount.depositFund(2000);
-        System.out.println("Balance = " + bankAccount.getBalance());
+        // BankAccount bartoszAccount = new BankAccount("456789123", 0.00
+        // , "John Smiht", "john.smith@gmail.com", "+48 456 789 123");
+        
+        System.out.println("-------------------");
 
-        bankAccount.withdrawFunds(1000);
-        System.out.println("Balance = " + bankAccount.getBalance());
+        System.out.println(bartoszAccount.getCustomerPhoneNumber());
+        System.out.println(bartoszAccount.getCustomerName());
+        System.out.println(bartoszAccount.getCustomerEmail());
+        System.out.println("-------------------");
+
+
+        // bartoszAccount.setAccountNumber("456789123"); 
+        // bartoszAccount.setBalance(0.00);
+        // bartoszAccount.setCustomerName("John Smith");
+        // bartoszAccount.setCustomerEmail("john.smith@gmail.com");
+        // bartoszAccount.setCustomerPhoneNumber("+48 456 789 123");
+
+        bartoszAccount.setBalance(5000);
+        System.out.println("Balance = " + bartoszAccount.getBalance());
+        System.out.println("-------------------");
+
+        bartoszAccount.depositFund(2000);
+        System.out.println("Balance = " + bartoszAccount.getBalance());
+        System.out.println("-------------------");
+
+        bartoszAccount.withdrawFunds(7000);
+        System.out.println("Balance = " + bartoszAccount.getBalance());
+        System.out.println("-------------------");
+
+        BankAccount safeAccount = new BankAccount("Tim", "tim@email.com", "34234");
+        System.out.println(safeAccount.getAccountNumber() + " name " + safeAccount.getCustomerName());
+
+        System.out.println("-------------------");
 
     }
 }
