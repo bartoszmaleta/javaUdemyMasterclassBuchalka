d8922b8dd5454b418ebcce2f1a5674dad4c82fec
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-08 11:39:31 +0100

    Add Person class

---
+++ src/oopPart1/personExe30/Main.java
@@ -0,0 +1,22 @@
+package oopPart1.personExe30;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setLastName("Smith");
+        System.out.println("fullName = " + person.getFullName());
+    }
+}
