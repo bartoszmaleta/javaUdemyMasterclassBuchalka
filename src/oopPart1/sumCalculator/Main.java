46c20a01acfc05d4d1232a68873d5cd854ce1cee
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-08 11:22:23 +0100

    Initial commit

---
+++ src/oopPart1/sumCalculator/Main.java
@@ -0,0 +1,13 @@
+package oopPart1.sumCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtraction = " + calculator.getSubtractionResult());
+    }
+}
