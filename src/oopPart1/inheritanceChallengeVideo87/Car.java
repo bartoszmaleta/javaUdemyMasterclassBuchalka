89fe321800f4fd14c91ede38f1b72b9c76150b39
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 13:59:25 +0100

    Add hasFuel variable

--- src/oopPart1/inheritanceChallengeVideo87/Car.java
+++ src/oopPart1/inheritanceChallengeVideo87/Car.java
@@ -1,14 +1,15 @@
 package oopPart1.inheritanceChallengeVideo87;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle {
     private String colour;
     private int wheels;
+    boolean hasFuel;
 
-
-    public Car(int size, String colour, int wheels) {
+    public Car(int size, String colour, int wheels, boolean hasFuel) {
         super(size, 1);
         this.colour = colour;
         this.wheels = wheels;
+        this.hasFuel = hasFuel;
     }
 
     public void handSteering() {
@@ -25,27 +26,28 @@ public class Car extends Vehicle{
 
     @Override
     public void move(int speed) {
-        System.out.println("Car.move() called. Car is moving at " + speed);
-        handSteering();
-        changingGeers();
-        drives();
-        super.move(speed);
+        if (hasFuel == true) {
+            System.out.println("Car.move() called. Car is moving at " + speed);
+            handSteering();
+            changingGeers();
+            drives();
+            super.move(speed);
+        }
     }
     // public String getColour() {
-    //     return colour;
+    // return colour;
     // }
 
     // public void setColour(String colour) {
-    //     this.colour = colour;
+    // this.colour = colour;
     // }
 
     // public int getWheels() {
-    //     return wheels;
+    // return wheels;
     // }
 
     // public void setWheels(int wheels) {
-    //     this.wheels = wheels;
+    // this.wheels = wheels;
     // }
 
-    
 }
