4fc749d535a4a3da18e78e84084835d488a979a8
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 14:20:23 +0100

    Add class with constructors and one method

--- src/oopPart1/inheritanceChallengeVideo87/ElectricCar.java
+++ src/oopPart1/inheritanceChallengeVideo87/ElectricCar.java
@@ -1,5 +1,23 @@
 package oopPart1.inheritanceChallengeVideo87;
 
 public class ElectricCar extends Car {
+    private int batteries;
+    private int energy;
+
+    public ElectricCar (int batteries, int energy, String name, int size, String colour, boolean hasFuel) {
+        super(name, size, colour, 4, false);
+        this.batteries = batteries;
+        this.energy = energy;
+    }
+    
+    @Override
+    public void move(int speed) {
+        if (this.energy > 0) {
+            System.out.println("ElectricCar.move() called. ElectricCar is moving at " + speed);
+            super.move(speed);
+            this.energy -= 10;
+            System.out.println("You used 10 energy.");
+        }
+    }
     
 }
