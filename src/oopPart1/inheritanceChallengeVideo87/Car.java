66bf983e84732801f89aebaf4225afac0f8a0873
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 13:56:45 +0100

    Add methods and constructors and instance variables

--- src/oopPart1/inheritanceChallengeVideo87/Car.java
+++ src/oopPart1/inheritanceChallengeVideo87/Car.java
@@ -1,5 +1,51 @@
 package oopPart1.inheritanceChallengeVideo87;
 
 public class Car extends Vehicle{
+    private String colour;
+    private int wheels;
+
+
+    public Car(int size, String colour, int wheels) {
+        super(size, 1);
+        this.colour = colour;
+        this.wheels = wheels;
+    }
+
+    public void handSteering() {
+        System.out.println("Car.handSteering() called");
+    }
+
+    public void changingGeers() {
+        System.out.println("Car.changingGeers() called)");
+    }
+
+    public void drives() {
+        System.out.println("Car.drives() called");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Car.move() called. Car is moving at " + speed);
+        handSteering();
+        changingGeers();
+        drives();
+        super.move(speed);
+    }
+    // public String getColour() {
+    //     return colour;
+    // }
+
+    // public void setColour(String colour) {
+    //     this.colour = colour;
+    // }
+
+    // public int getWheels() {
+    //     return wheels;
+    // }
+
+    // public void setWheels(int wheels) {
+    //     this.wheels = wheels;
+    // }
+
     
 }
