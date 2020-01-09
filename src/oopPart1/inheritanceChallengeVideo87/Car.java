60f5532bedb3820b48335b9a8abee931b04716bd
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 13:42:20 +0100

    Add Vehicle class constructor and getters/setters

--- src/oopPart1/inheritanceChallengeVideo87/Car.java
+++ src/oopPart1/inheritanceChallengeVideo87/Car.java
@@ -1,5 +1,5 @@
 package oopPart1.inheritanceChallengeVideo87;
 
 public class Car extends Vehicle{
-
+    
 }
--- src/oopPart1/inheritanceChallengeVideo87/Vehicle.java
+++ src/oopPart1/inheritanceChallengeVideo87/Vehicle.java
@@ -1,5 +1,21 @@
 package oopPart1.inheritanceChallengeVideo87;
 
 public class Vehicle {
-    
+    private int size;
+
+    public Vehicle(int size) {
+        this.size = size;
+    }
+
+    public void move(int speed) {
+        System.out.println("Vehicle.move() called. Vehicle is moving at " + speed);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
