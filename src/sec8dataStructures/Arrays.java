8002540b1aadef31278263c60266557f8c84b9d6
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-13 11:00:00 +0100

    Initial commit for data structures section

---
+++ src/sec8dataStructures/Arrays.java
@@ -0,0 +1,13 @@
+package sec8dataStructures;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myIntArray= new int[10];
+        myIntArray[5] = 50;
+        System.out.println(myIntArray); // reference in memory
+        System.out.println(myIntArray[5]); // 50
+
+        double[] myDoubleArray = new double[10];
+        
+    }
+}
