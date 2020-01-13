4739819b4482eb99ec72e8324898793ea5ccd70a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-13 15:48:39 +0100

    Add reference of int and arrayscd

--- src/sec8dataStructures/ReferenceTypes101.java
+++ src/sec8dataStructures/ReferenceTypes101.java
@@ -12,16 +12,33 @@ public class ReferenceTypes101 {
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("anotherIntValue = " + anotherIntValue);
 
+        System.out.println("-----------");
         anotherIntValue++;
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("anotherIntValue = " + anotherIntValue);
 
+        System.out.println("-----------");
         int[] myIntArray = new int[5];
         int[] anotherArray = myIntArray;
-
         System.out.println("myIntArray = " + Arrays.toString(myIntArray));
         System.out.println("anotherArray = " + Arrays.toString(anotherArray));
-        
+
+        System.out.println("-----------");
+        anotherArray[0] = 1;
+        System.out.println("after change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray = " + Arrays.toString(anotherArray));
+
+        System.out.println("-----------");
+        anotherArray = new int[] {4, 5, 6, 7, 8};
+        modifyArray(myIntArray);
+        System.out.println("after modify myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after modify anotherArray = " + Arrays.toString(anotherArray));
+
+    }
+
+    private static void modifyArray(int[] arrayToModify) {
+        arrayToModify[0] = 2;
+        arrayToModify = new int[] {1, 2, 3, 4, 5};
     }
 }
