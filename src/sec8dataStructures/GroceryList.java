0d5d9018539c8593587447eef4445421d356e8e7
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-14 10:18:57 +0100

    Add add()  print() modify() remove() of ArrayList

--- src/sec8dataStructures/GroceryList.java
+++ src/sec8dataStructures/GroceryList.java
@@ -9,4 +9,23 @@ public class GroceryList {
     public void addGroceryItem(String item) {
         groceryList.add(item);
     }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery List");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i = 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+
 }
